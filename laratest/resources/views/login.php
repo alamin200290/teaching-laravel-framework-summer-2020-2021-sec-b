d1a87fe80d09c885f1f398a60d02eb0fa9d41ae1
sarwar_sikder <amin.se.aiub@gmail.com>
2021-06-03 22:50:46 +0600

    project setup & basics

---
+++ laratest/resources/views/login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login Page</title>
+</head>
+<body>
+	<h1>This is login page...</h1>
+	<a href="/register">Signup</a>
+</body>
+</html>
---
+++ laratest/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', function () {
+    //echo "it works...";
+	return view('login');
+});
+
+
+Route::get('/register', function(){
+	echo "this is signup page...";
+});
